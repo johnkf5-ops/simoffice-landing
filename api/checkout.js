198cbd0394eb18ebe765331ef412ee1ea5adab0b
John Knopf <johnknopf@Johns-MacBook-Pro-2.local>
2026-03-23 17:21:03 -0700

    feat: enable promo codes on checkout

--- api/checkout.js
+++ api/checkout.js
@@ -45,6 +45,7 @@ module.exports = async (req, res) => {
       'line_items[0][price]': PRICE_ID,
       'line_items[0][quantity]': 1,
       'subscription_data[trial_period_days]': 1,
+      allow_promotion_codes: 'true',
       success_url: `${origin}/download.html?session_id={CHECKOUT_SESSION_ID}`,
       cancel_url: `${origin}/#download`,
     });
